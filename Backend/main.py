dcb2ae780a56031c594f7b0042458340c7a11bbe
Thembelihle <msangelz58@gmail.com>
2025-07-31 17:03:31 +0200

    Update CORS settings in main.py to remove outdated frontend origin
    - Removed the frontend origin "https://frontend-7td4.onrender.com" from the CORS configuration to streamline allowed origins and ensure alignment with current deployment.

--- Backend/main.py
+++ Backend/main.py
@@ -80,7 +80,7 @@ flask_app.config['JWT_HEADER_TYPE'] = 'Bearer'
 # Initialize CORS properly in one place
 CORS(flask_app, 
      resources={r"/api/*": {
-         "origins": ["http://localhost:3000", "https://backened-h577.onrender.com", "http://127.0.0.1:3000","https://frontend-7td4.onrender.com", "https://frontend-d8o0.onrender.com"],
+         "origins": ["http://localhost:3000", "https://backened-h577.onrender.com", "http://127.0.0.1:3000", "https://frontend-d8o0.onrender.com"],
          "supports_credentials": True,
          "allow_headers": ["Content-Type", "Authorization", "Access-Control-Allow-Origin","X-Requested-With"],
          #"expose_headers": ["Authorization"],
