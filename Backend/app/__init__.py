4636a8abf62ff21f3f7c78e22c994b299389d273
Nathi2266 <Nathiradebe71@gmail.com>
2025-08-05 07:43:25 +0200

    Add notification preference blueprint registration in Flask app
    - Registered the notification preference blueprint as 'notifications_bp' in the main application to enhance routing and organization.
    - This addition supports the management of user notification preferences within the backend application.

--- Backend/app/__init__.py
+++ Backend/app/__init__.py
@@ -6,6 +6,7 @@ from datetime import timedelta
 from dotenv import load_dotenv
 from flask_cors import CORS
 from app.routes.topup import topup_bp
+from app.routes.notification_preference import notifications_bp
 
 # Load environment variables
 load_dotenv()
